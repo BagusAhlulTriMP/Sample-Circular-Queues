c7811c70c651eec1a426afa16130dffa9cf33166
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-12-14 16:52:49 +0700

    add method insert

--- Sample Circular Queues/Program.cs
+++ Sample Circular Queues/Program.cs
@@ -9,7 +9,18 @@ namespace Sample_Circular_Queues
         int[] queue_array = new int[5];
         public Queues()
         {
-
+            /*Initializing the values of the  variables REAR and FRONT to -1 to indicate that the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            /* This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
         }
     }
 }
